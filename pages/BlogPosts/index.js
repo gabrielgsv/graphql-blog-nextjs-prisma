424b82faa9353782354831347031a9158f04d4c7
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-02-15 13:24:47 -0300

    add header Image

--- pages/BlogPosts/index.js
+++ pages/BlogPosts/index.js
@@ -1,10 +1,12 @@
 import { useQuery, useMutation } from '@apollo/client'
 import { Flex } from '@chakra-ui/react';
+import Image from "next/image"
 import { useEffect, useState } from 'react';
 
 import { GET_BLOGPOSTS, DELETE_BLOGPOST } from "./queries"
 import NewPost from "./NewPost";
 import ListPosts from "./ListPosts";
+import image from "../../images/next-graphql-apollo.jpg"
 
 const BlogPosts = () => {
   const { loading, error, data } = useQuery(GET_BLOGPOSTS)
@@ -26,7 +28,14 @@ const BlogPosts = () => {
 
   return (
     <>
-      <Flex width={"100vw"} alignItems={"center"} marginTop={20} flexDirection={"column"}>
+      <Flex width={"100vw"} alignItems={"center"} marginTop="-60px" flexDirection={"column"}>
+        <Image
+          src={image}
+          alt="Next Graphql and Apollo"
+          width={600}
+          height={350}
+          quality={100}
+        />
         <NewPost text={text} setText={setText} />
         <ListPosts posts={posts} setPosts={setPosts} />
       </Flex>
